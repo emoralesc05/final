59ffa4735086e4d1beb21673604a3a2e32aa3c94
emoralesc05 <ipad.emoralesc@gmail.com>
2026-03-04 17:04:47 -0500

    Refactor server to use environment variable for port

--- server.js
+++ server.js
@@ -81,6 +81,8 @@ app.get("/test", async (req, res) => {
   res.json({ data, error });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
